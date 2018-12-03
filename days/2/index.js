5436479eec117933617a10a69c7391df1d5805cf
Dean Kerr <dkerr@scottlogic.co.uk>
2018-12-03 13:50:22 +0000

    feature: re-add no confusing arrow lint rule

--- days/2/index.js
+++ days/2/index.js
@@ -18,7 +18,7 @@ fs.readFile('./input.txt', 'utf-8', (err, data) => {
 
   // Part two
   ids.forEach(id => ids.forEach((other) => {
-    if (id.split('').reduce((memo, char, index) => char === other[index] ? memo + 1 : memo, 0) === id.length - 1) {
+    if (id.split('').reduce((memo, char, index) => (char === other[index] ? memo + 1 : memo), 0) === id.length - 1) {
       console.log(id, other);
     }
   }));
