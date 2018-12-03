cf2e25214dd9822fbad7b47a233a5ae911783110
Dean Kerr <dean_k@live.co.uk>
2018-12-03 22:39:06 +0000

    feature: day 3 part two implementation

--- days/3/index.js
+++ days/3/index.js
@@ -1,34 +1,42 @@
 const fs = require('fs');
 
 fs.readFile('./input.txt', 'utf-8', (err, data) => {
-  const gridSize = 1007;
-  const grid = [...Array(gridSize)].map(() => [...Array(gridSize)].map(() => 0));
-  // const grid = {}
+  const grid = {};
 
-  const claims = data.split('\r\n').map(row => {
+  const claims = data.split('\r\n').map((row) => {
     const [id, x, y, w, h] = /#(\d+)\s@\s(\d+),(\d+): (\d+)x(\d+)/.exec(row).slice(1);
-    return { id: Number(id), x: Number(x), y: Number(y), w: Number(w), h: Number(h) };
+    return {
+      id,
+      x: Number(x),
+      y: Number(y),
+      w: Number(w),
+      h: Number(h),
+    };
   });
 
-  claims.forEach(claim => {
-    for (let row = claim.x; row < claim.x + claim.w; row++) {
-      for (let col = claim.y; col < claim.y + claim.h; col++) {
-        grid[row][col] = grid[row][col] ? grid[row][col] + 1 : 1;
+  claims.forEach((claim) => {
+    // Fill in claim horizontally rightwards, one row at a time
+    for (let col = claim.y; col < claim.y + claim.h; col++) {
+      for (let row = claim.x; row < claim.x + claim.w; row++) {
+        const isOverlapping = Boolean(grid[`${row},${col}`]);
+        grid[`${row},${col}`] = isOverlapping ? grid[`${row},${col}`] + 1 : 1;
       }
     }
   });
 
-  console.log(grid.reduce((memo, column) => memo + column.filter(value => value > 1).length, 0));
+  // Part One
+  console.log(Object.values(grid).filter(value => value > 1).length);
 
-  // claims.forEach(claim => {
-  //   for (let row = claim.x; row < claim.x + claim.h; row++) {
-  //     for (let col = claim.y; col < claim.y + claim.w; col++) {
-  //       grid[`${row}${col}`] = grid[`${row}${col}`] ? grid[`${row}${col}`] + 1 : 1;
-  //     }
-  //   }
-  // });
+  // Part Two
+  console.log(claims.find((claim) => {
+    for (let col = claim.y; col < claim.y + claim.h; col++) {
+      for (let row = claim.x; row < claim.x + claim.w; row++) {
+        if (grid[`${row},${col}`] > 1) {
+          return false;
+        }
+      }
+    }
 
-  // for (let row = 0; row < grid.length; row++) {
-  //   console.log(grid[row]);
-  // }
+    return true;
+  }));
 });
